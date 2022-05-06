1e8ea9df784058d3b6bf78c2c3ad1fb05315efb6
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-05-06 21:58:09 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,45 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+int Train::getLength() {
+  int w = 0, i = 0;
+  first->light = true;
+  Cage *t = first;
+  while (true) {
+    ++i;
+    t = t->next;
+    ++countOp;
+    if (t->light) {
+      w = i;
+      t->light = false;
+      int j = --i;
+      for (j; j >= 0; --j) {
+        t = t->prev;
+        ++countOp;
+        --i;
+      }
+      if (t->light == false) {
+        return w;
+      }
+    }
+  }
+}
+int Train::getOpCount() {
+  return countOp;
+}
+  void Train::addCage(bool light) {
+    if (first && last) {
+      last->next = add(last);
+      last->next = add(light);
+      last->next->prev = last;
+      last = last->next;
+      if (!first->next) {
+        first->prev = last;
+      }
+      if (!last->prev) {
+        last->next = first;
+      }
+    } else {
+      first = add(light);
+      last = first;
+    }
+}
