f5d99fa53782e5de364d9a0fc8543f7ba95d4071
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-05-06 22:10:45 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -11,7 +11,7 @@ int Train::getLength() {
     if (t->light) {
       w = i;
       t->light = false;
-      int j = i;
+      int j = --i;
       for (j; j >= 0; --j) {
         t = t->prev;
         ++countOp;
@@ -26,20 +26,20 @@ int Train::getLength() {
 int Train::getOpCount() {
   return countOp;
 }
-  void Train::addCage(bool light) {
-    if (first && last) {
-      last->next = add(last);
-      last->next = add(light);
-      last->next->prev = last;
-      last = last->next;
-      if (!first->next) {
-        first->prev = last;
-      }
-      if (!last->prev) {
-        last->next = first;
-      }
-    } else {
-      first = add(light);
-      last = first;
+void Train::addCage(bool light) {
+  if (first && last) {
+    last->next = add(last);
+    last->next = add(light);
+    last->next->prev = last;
+    last = last->next;
+    if (!first->next) {
+      first->prev = last;
     }
+    if (!last->prev) {
+      last->next = first;
+    }
+  } else {
+    first = add(light);
+    last = first;
+  }
 }
